5ee3072b0ee1a575fedbad84453bff0781882eb6
Le Hoang Anh <lehoanganh@Les-MacBook-Pro.local>
2026-05-06 21:25:46 +0700

    statr

--- src/starting/Starting.java
+++ src/starting/Starting.java
@@ -1,5 +1,25 @@
 package starting;
 
-public class Starting {
+import javax.swing.SwingUtilities;
+import gui.DangNhap_GUI;
+import connectDB.ConnectDB;
 
+public class Starting {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                // Kết nối cơ sở dữ liệu
+                ConnectDB.getInstance().connect();
+
+                // Mở form đăng nhập
+                new DangNhap_GUI().setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+                javax.swing.JOptionPane.showMessageDialog(null,
+                        "Không thể kết nối cơ sở dữ liệu!",
+                        "Lỗi",
+                        javax.swing.JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
 }
